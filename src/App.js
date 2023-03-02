8d4502459225940e6dcdd842b3a55315e44574c6
Bharath <bharathg1252001@gmail.com>
2023-03-02 22:24:18 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+// Take Home Assignment
 
-function App() {
+// 1. Write reducer for UPDATE_EXPENSE action.
+// 2. Make Month and Year filters functional.
+// 3. Add options in Navbar to move through the application
+// 4. Handle wrong credentials login error message
+// 5. Create Reset Filters button
+
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import { PATHS } from "./paths";
+import Login from "./components/Login/Login";
+import AddExpense from "./components/AddExpense/AddExpense";
+import ManageExpense from "./components/ManageExpense/ManageExpense";
+import PrivateRoutes from "./routes/PrivateRoutes";
+import RestrictedRoutes from "./routes/RestrictedRoutes";
+
+export const centeredStyle = {
+  width: "100%",
+  height: "100vh",
+  display: "grid",
+  placeItems: "center",
+  background: "dodgerblue",
+};
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<PrivateRoutes />}>
+        <Route path={PATHS.ADD_EXPENSE} element={<AddExpense />} />
+        <Route path={PATHS.MANAGE_EXPENSE} element={<ManageExpense />} />
+      </Route>
+
+      <Route path="/" element={<RestrictedRoutes />}>
+        <Route path={PATHS.LOGIN} element={<Login />} />
+      </Route>
+    </Routes>
   );
-}
+};
 
 export default App;
